4aa340a263e5a4a56aac0d4e64a9e251d7dee496
Helios <vakkendwarf@gmail.com>
2018-10-10 12:25:47 +0200

    added insert at index

--- node_recursive.py
+++ node_recursive.py
@@ -2,19 +2,44 @@ class Node(object):
     def __init__(self, data, nextt):
         self.__data = data
         self.__next = nextt
+        self.__index = 0
+
+    def index_list(self):
+        if self.__next is None:
+            return
+        self.__next.set_index(self.__index+1)
+        self.__next.index_list()
+
+    def set_index(self, newindex):
+        self.__index = newindex
+
+    def insert_at_index(self, index, data):
+        if self.__next is None:
+            return print("Nie znaleziono tego indeksa.")
+        elif str(self.__index) == str(index):
+            self.__data = data
+            self.index_list()
+            return
+        self.__next.insert_at_index(index, data)
 
     def add_to_end(self, newhead):
         if self.__next is None:
             self.__next = Node(newhead, None)
+            self.index_list()
             return
         self.__next.add_to_end(newhead)
 
+    def delete_from_end(self):
+        if self.__next.__next is None:
+            self.__next = None
+            return
+        self.__next.delete_from_end()
+
     def print_list(self):
-        print(self.__data)
+        print("Index: " + str(self.__index) + ", Data: " + str(self.__data))
         if self.__next is None:
             return
         self.__next.print_list()
-
 
     def get_next(self):
         return self.__next
--- testing_grounds.py
+++ testing_grounds.py
@@ -13,3 +13,11 @@ node1.add_to_end(10)
 node1.add_to_end(120)
 
 node1.print_list()
+
+node1.delete_from_end()
+
+node1.print_list()
+
+node1.insert_at_index(1, 225)
+
+node1.print_list()
